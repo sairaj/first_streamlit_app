15e8e5b86197cf484b20efba323b6f097241b9c5
DSR <sairaj.nitw@gmail.com>
2023-05-31 05:16:15 +0530

    Update streamlit_app.py
    used text and write functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.subheader('Breakfast Menu')
 
+streamlit.text('Omega3 and blueberry oatmeal')
+
+streamlit.text('Hard boiled free range egg')
+
+streamlit.write('writing with write function')
